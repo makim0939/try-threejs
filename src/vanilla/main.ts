87c852d81ba5e0386110e33df88083d3bcb269da
makim0939 <95673492+makim0939@users.noreply.github.com>
2025-03-25 09:52:28 +0900

    clean: comment out unused steps

--- src/vanilla/main.ts
+++ src/vanilla/main.ts
@@ -1,6 +1,6 @@
-import { rotationCube } from "../threeFunctions/01_rotationCube";
-import { importGlb } from "../threeFunctions/02_importGlb";
-import { orbitControls } from "../threeFunctions/03_orbitControls";
+// import { rotationCube } from "../threeFunctions/01_rotationCube";
+// import { importGlb } from "../threeFunctions/02_importGlb";
+// import { orbitControls } from "../threeFunctions/03_orbitControls";
 import { arjs } from "../threeFunctions/04_arjs";
 
 const root = document.getElementById("root");
